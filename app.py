850ea937c5e79b5462037884ca46a28957412eaf
DungGLA <adunghver23@gmail.com>
2021-11-11 16:46:42 +0700

    Added postgresql environment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +12,8 @@ from resource.store import Store, StoreList
 
 app = Flask(__name__)
 
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLACHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 # To know when an object had changed, but not been saved to the db, the extension flask SQLAchemy was tracking every
 # change that we made to the SQLAchemy session and that took some resources.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
