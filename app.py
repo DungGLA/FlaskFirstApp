627e8655aa08e19a4de02f5c002d0056acc5a8f3
DungGLA <adunghver23@gmail.com>
2021-11-11 14:30:38 +0700

    change run app

--- app.py
+++ app.py
@@ -27,10 +27,6 @@ api.add_resource(StoreList, "/stores")
 
 api.add_resource(UserRegister, "/register")
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 db.init_app(app)
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
